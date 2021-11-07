c38fb4f27c32ccdc7774b546aa3fdff3bad72669
Neil <neilbalch@gmail.com>
2021-11-06 18:11:16 -0700

    Fix function prototype syntax

--- include/game_code.h
+++ include/game_code.h
@@ -4,10 +4,18 @@
 #include "util.h"
 #pragma code-name ("CODE2")
 
-void reset();
+// Function prototypes with no parameters must be declared with a `void`
+// parameter: https://github.com/cc65/cc65/issues/1591#issuecomment-962532711
 
-void do_logic();
+// Called once upon MPU reset
+void reset(void);
 
-void fill_vram();
+// Called once per frame to perform all necessary game logic, without writing
+// to the GPU frame buffer.
+void do_logic(void);
+
+// Called after completion of the do_logic() function and the next GPU interrupt
+// indicating it is safe to write to the GPU frame buffer.
+void fill_vram(void);
 
 #endif
--- src/game_code.c
+++ src/game_code.c
@@ -1,21 +1,6 @@
-// #include "game_code.h"
-// #include "util.h"
+#include "util.h"
+#include "game_code.h"
 #pragma code-name ("CODE2")
-
-#ifndef UTIL_H
-#define UTIL_H
-
-#define nop()  __asm__ ("NOP") // NO Operation opcode (http://www.6502.org/users/andre/65k/af65002/af65002opcodes.html)
-#define sei()  __asm__ ("SEI") // Enable interrupts (set flag)
-#define cli()  __asm__ ("CLI") // Disable interrupts (reset flag)
-
-#endif
-
-void reset();
-
-void do_logic();
-
-void fill_vram();
 
 void reset() {
   nop();
