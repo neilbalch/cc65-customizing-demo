94647cf4d08831eb5e85b985f0844f29e0949395
Neil <neilbalch@gmail.com>
2021-11-15 18:57:02 -0800

    Replace `/ 64` with `>> 6`

--- backend_src/Q9_6.c
+++ backend_src/Q9_6.c
@@ -29,10 +29,10 @@ sint16_t Q9_6_to_sint16(Q9_6 num) {
 
 Q9_6 mul(Q9_6 n1, Q9_6 n2) {
     // TODO
-    return ((sint32_t)n1 * (sint32_t)n2) / 64;
+    return ((sint32_t)n1 * (sint32_t)n2) >> 6;
 }
 
 Q9_6 div(Q9_6 n1, Q9_6 n2) {
     // TODO
-    return (n1 / (n2 / 64));
+    return (n1 / (n2 >> 6));
 }
