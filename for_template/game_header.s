0a981751c7faf8770f4f118ae5fe34ffa863254d
E4tHam <ethan@sifferman.dev>
2021-11-19 16:29:08 -0800

    fixed memory bug, added rodata segment, fixed comments

--- for_template/game_header.s
+++ for_template/game_header.s
@@ -1,5 +1,5 @@
-; Imports the code version identifiers into each identifiable memory segment
+; Imports the game header
 
 .segment "ROM_GAME_HEADER"
 .incbin "game_header.txt"
-.byte 0 ; Spacer/null-terminator byte
+.byte 0 ; null-terminator
--- headers/vram.h
+++ headers/vram.h
@@ -1,6 +1,6 @@
 // Declare all types and variables associated with modifying VRAM
 
-// For more details, see: https://arcade.ucsbieee.org/guides/gpu
+// For more details, see: https://arcade.ucsbieee.org/guides/gpu/
 #ifndef __VRAM_H
 #define __VRAM_H
 
@@ -37,38 +37,38 @@ extern background_palette_t background_palette;
 extern object_t OBM[32]; // OBject Memory
 
 // Tile byte flag masks
-#define COLOR_ALT_MASK          0x80
-#define HFLIP_MASK              0x40
-#define VFLIP_MASK              0x20
-#define PATTERN_ADDRESS_MASK    0x1f
+#define COLOR_ALT_MASK          ((uint8_t)0x80)
+#define HFLIP_MASK              ((uint8_t)0x40)
+#define VFLIP_MASK              ((uint8_t)0x20)
+#define PATTERN_ADDRESS_MASK    ((uint8_t)0x1f)
 
 // Color masks for colors
-#define BLACK_C_MASK     0x0
-#define BLUE_C_MASK      0x1
-#define GREEN_C_MASK     0x2
-#define CYAN_C_MASK      0x3
-#define RED_C_MASK       0x4
-#define MAGENTA_C_MASK   0x5
-#define YELLOW_C_MASK    0x6
-#define WHITE_C_MASK     0x7
+#define BLACK_C_MASK     ((uint8_t)0x0)
+#define BLUE_C_MASK      ((uint8_t)0x1)
+#define GREEN_C_MASK     ((uint8_t)0x2)
+#define CYAN_C_MASK      ((uint8_t)0x3)
+#define RED_C_MASK       ((uint8_t)0x4)
+#define MAGENTA_C_MASK   ((uint8_t)0x5)
+#define YELLOW_C_MASK    ((uint8_t)0x6)
+#define WHITE_C_MASK     ((uint8_t)0x7)
 
 // Default color masks
-#define BLACK_C0_MASK    BLACK_C_MASK   << 0
-#define BLUE_C0_MASK     BLUE_C_MASK    << 0
-#define GREEN_C0_MASK    GREEN_C_MASK   << 0
-#define CYAN_C0_MASK     CYAN_C_MASK    << 0
-#define RED_C0_MASK      RED_C_MASK     << 0
-#define MAGENTA_C0_MASK  MAGENTA_C_MASK << 0
-#define YELLOW_C0_MASK   YELLOW_C_MASK  << 0
-#define WHITE_C0_MASK    WHITE_C_MASK   << 0
+#define BLACK_C0_MASK    (BLACK_C_MASK   << 0)
+#define BLUE_C0_MASK     (BLUE_C_MASK    << 0)
+#define GREEN_C0_MASK    (GREEN_C_MASK   << 0)
+#define CYAN_C0_MASK     (CYAN_C_MASK    << 0)
+#define RED_C0_MASK      (RED_C_MASK     << 0)
+#define MAGENTA_C0_MASK  (MAGENTA_C_MASK << 0)
+#define YELLOW_C0_MASK   (YELLOW_C_MASK  << 0)
+#define WHITE_C0_MASK    (WHITE_C_MASK   << 0)
 // Alt color masks
-#define BLACK_C1_MASK    BLACK_C_MASK   << 3
-#define BLUE_C1_MASK     BLUE_C_MASK    << 3
-#define GREEN_C1_MASK    GREEN_C_MASK   << 3
-#define CYAN_C1_MASK     CYAN_C_MASK    << 3
-#define RED_C1_MASK      RED_C_MASK     << 3
-#define MAGENTA_C1_MASK  MAGENTA_C_MASK << 3
-#define YELLOW_C1_MASK   YELLOW_C_MASK  << 3
-#define WHITE_C1_MASK    WHITE_C_MASK   << 3
+#define BLACK_C1_MASK    (BLACK_C_MASK   << 3)
+#define BLUE_C1_MASK     (BLUE_C_MASK    << 3)
+#define GREEN_C1_MASK    (GREEN_C_MASK   << 3)
+#define CYAN_C1_MASK     (CYAN_C_MASK    << 3)
+#define RED_C1_MASK      (RED_C_MASK     << 3)
+#define MAGENTA_C1_MASK  (MAGENTA_C_MASK << 3)
+#define YELLOW_C1_MASK   (YELLOW_C_MASK  << 3)
+#define WHITE_C1_MASK    (WHITE_C_MASK   << 3)
 
 #endif
